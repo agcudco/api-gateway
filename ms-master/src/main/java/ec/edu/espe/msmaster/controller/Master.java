36a1795e8b53c19e62886135df8abecd4418bc4b
agcudco <agcudco@espe.edu.ec>
2024-11-29 17:50:20 -0500

    clase master

--- ms-master/src/main/java/ec/edu/espe/msmaster/controller/Master.java
+++ ms-master/src/main/java/ec/edu/espe/msmaster/controller/Master.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("master")
+@RequestMapping("/master")
 public class Master {
 
     @GetMapping("/")
